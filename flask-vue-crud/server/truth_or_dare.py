29a2c546f9dfcf2308d9f735478db71a139205d7
qinran6271 <qinranwa@buffalo.edu>
2023-06-15 14:16:30 -0500

    Update truth_or_dare.py

--- flask-vue-crud/server/truth_or_dare.py
+++ flask-vue-crud/server/truth_or_dare.py
@@ -10,39 +10,39 @@ def truth():
 
     # 以下是真心话内容
     # # 获取请求参数
-    # data_type = request.args.get('type')
-    # person = request.args.get('person')
-    # num = int(request.args.get('num'))
-    # y_or_n = request.args.get('yOrN')
+    data_type = request.args.get('type')
+    person = request.args.get('person')
+    num = int(request.args.get('num'))
+    y_or_n = request.args.get('yOrN')
 
     # 构建查询参数
-    # query = {
-    #     "type": data_type,
-    #     "person": person,
-    #     "num": num,
-    #     "yOrN": y_or_n
-    # }
-    # result = truth_dare.find_one(query)
-    # keys_to_remove = ['_id', 'type', 'person', 'num', 'yOrN']
-    # for key in keys_to_remove:
-    #     result.pop(key, None)
-    
-    # return jsonify(result)
-
-    # 以下是测试，可删除
     query = {
-        "type": "真心话",
-        "person": "我",
-        "num": 11,
-        "yOrN": "NO"
+        "type": data_type,
+        "person": person,
+        "num": num,
+        "yOrN": y_or_n
     }
-
     result = truth_dare.find_one(query)
     keys_to_remove = ['_id', 'type', 'person', 'num', 'yOrN']
     for key in keys_to_remove:
         result.pop(key, None)
-    # result.pop('_id', None)
-    print(result)
+    
     return jsonify(result)
+
+    # 以下是测试，可删除
+    # query = {
+    #     "type": "真心话",
+    #     "真心话": "我",
+    #     "num": 11,
+    #     "yOrN": "NO"
+    # }
+
+    # result = truth_dare.find_one(query)
+    # keys_to_remove = ['_id', 'type', 'person', 'num', 'yOrN']
+    # for key in keys_to_remove:
+    #     result.pop(key, None)
+    # # result.pop('_id', None)
+    # print(result)
+    # return jsonify(result)
     
    
