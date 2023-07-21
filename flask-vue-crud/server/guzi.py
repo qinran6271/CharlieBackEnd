aa6b712fa279c1ec422f4d636d6083bb93ddf8fe
sunqi <18238776874@163.com>
2023-07-21 18:27:27 +0800

    谷子按照时间和价格排序接口

--- flask-vue-crud/server/guzi.py
+++ flask-vue-crud/server/guzi.py
@@ -11,6 +11,7 @@ def merchandise():
         # 处理附带参数的情况
         type = request.args.get('type')  # 谷子类型：【徽章】
         name = request.args.get('name')  # 谷子名字：【满赠透卡】
+        sign = request.args.get('sign')  # 谷子名字：【满赠透卡
         query = {
         }
         if not type is None:
@@ -21,8 +22,18 @@ def merchandise():
             condition['$regex'] = name
             query.update({"name": condition})
 
-        print(query)
-        data = guzi.find(query)
+        if not sign is None:
+            print(sign)
+            if sign == "priceASC":
+                data = guzi.find(query).sort("price", 1)
+            if sign == "priceDESC":
+                data = guzi.find(query).sort("price", -1)
+            if sign == "timeASC":
+                data = guzi.find(query).sort("release_time", 1)
+            if sign == "timeDESC":
+                data = guzi.find(query).sort("release_time", -1)
+        else:
+            data = guzi.find(query)
         data = list(data)
         # print(data)
         result = []
